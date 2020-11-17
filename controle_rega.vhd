2637c3f990cd4937cd08494854b65b58671c688f
Lucas Mafra <lucas.mafra95@gmail.com>
2020-11-17 12:29:28 -0300

    Add counter to irrigate for 1s

--- controle_rega.vhd
+++ controle_rega.vhd
@@ -34,9 +34,13 @@ architecture arch of controle_rega is
       i_clock: in std_logic;
       i_reset: in std_logic;
       i_ligar: in std_logic;
-      i_valor_temporizador: in std_logic;
-      i_fim_temporizador: in std_logic;
+      i_fim_rega: in std_logic;
+      i_fim_repouso: in std_logic;
       i_girou_servomotor: in std_logic;
+      i_medida_pronta: in std_logic;
+      i_abaixo_threshold: in std_logic;
+      o_medir: out std_logic;
+      o_repousando: std_logic;
       o_alternar_vaso: out std_logic;
       o_abre_valvula: out std_logic;
       o_conta_espera_giro_servomotor: out std_logic;
@@ -62,10 +66,13 @@ architecture arch of controle_rega is
   signal s_alternar_vaso: std_logic;
   signal s_conta_espera_giro_servomotor: std_logic;
   signal s_girou_servomotor: std_logic;
+  signal s_abre_valvula: std_logic;
+  signal s_fim_rega: std_logic;
   
 begin
   s_reset_temporizador <= i_reset or i_ligar;
-
+  o_abre_valvula <= s_abre_valvula;
+ 
   temporizador_0: temporizador
     generic map (
      M => 100000000 / velocidade_simulacao, -- 2s
@@ -83,11 +90,15 @@ begin
       i_clock => i_clock,
       i_reset => i_reset,
       i_ligar => i_ligar,
-      i_valor_temporizador => s_temporizador,
-      i_fim_temporizador => s_fim_temporizador,
+      i_fim_rega => s_fim_rega,
+      i_fim_repouso =>
       i_girou_servomotor => s_girou_servomotor,
+      i_medida_pronta =>
+      i_abaixo_threshold =>
+      o_medir =>
+      o_repousando =>
       o_alternar_vaso => s_alternar_vaso,
-      o_abre_valvula => o_abre_valvula,
+      o_abre_valvula => s_abre_valvula,
       o_conta_espera_giro_servomotor => s_conta_espera_giro_servomotor,
       db_estado => open
     );
@@ -103,6 +114,19 @@ begin
       conta => s_alternar_vaso,
       Q => o_vaso,
       fim => open
+    );
+
+  contador_rega_0: contador_m
+    generic map (
+      M => 50000000 / velocidade_simulacao, -- rega por 1s,
+      N => 32
+    )
+    port map (
+      clock => i_clock,
+      zera => i_reset,
+      conta => s_abre_valvula,
+      Q => open,
+      fim => s_fim_rega
     );
 
   contador_espera_giro_servomotor_0: contador_m
