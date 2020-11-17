de9603bf2bd1496fe972a24f3edc708d154cf836
Lucas Mafra <lucas.mafra95@gmail.com>
2020-11-17 12:21:31 -0300

    Modify controle_rega_uc to behave according to sensor_umidade_solo

--- controle_rega_uc.vhd
+++ controle_rega_uc.vhd
@@ -7,9 +7,13 @@ entity controle_rega_uc is
     i_clock: in std_logic;
     i_reset: in std_logic;
     i_ligar: in std_logic;
-    i_valor_temporizador: in std_logic;
-    i_fim_temporizador: in std_logic;
+    i_fim_rega: in std_logic;
+    i_fim_repouso: in std_logic;
     i_girou_servomotor: in std_logic;
+    i_medida_pronta: in std_logic;
+    i_abaixo_threshold: in std_logic;
+    o_medir: out std_logic;
+    o_repousando: std_logic;
     o_alternar_vaso: out std_logic;
     o_abre_valvula: out std_logic;
     o_conta_espera_giro_servomotor: out std_logic;
@@ -19,7 +23,7 @@ end;
 
 architecture arch of controle_rega_uc is
 
-  type tipo_estado is (inicial, rega, repouso, alterna_vaso, espera_giro_servomotor);
+  type tipo_estado is (inicial, trigga_medida, espera_medida, rega, repouso, alterna_vaso, espera_giro_servomotor);
   signal Eatual: tipo_estado;
   signal Eprox: tipo_estado;
   
@@ -38,24 +42,31 @@ begin
   begin
     case Eatual is
       when inicial => if i_ligar = '1' then
-                        Eprox <= repouso;
+                        Eprox <= trigga_medida;
                       else
                         Eprox <= inicial;
                       end if;
 
-      when repouso => if i_valor_temporizador = '1' then
-                        Eprox <= rega;
-                      elsif i_fim_temporizador = '1' then
-                        Eprox <= alterna_vaso;
+      when trigga_medida => Eprox <= espera_medida;
+
+      when espera_medida => if i_abaixo_threshold = '1' then
+                              Eprox <= rega;
+                            else
+                              Eprox <= repouso;
+
+      when rega => if i_fim_rega = '1' then
+                     Eprox <= repouso;
+                   else
+                     Eprox <= rega;
+                   end if;
+                            
+      when repouso => if i_abaixo_threshold = '1' then
+                        Eprox <= trigga_medida;
                       else
-                        Eprox <= repouso;
+                        Eprox <= alterna_vaso;
                       end if;
 
-      when rega => if i_valor_temporizador = '1' then
-                     Eprox <= rega;
-                   else
-                     Eprox <= repouso;
-                   end if;
+      
 
       when alterna_vaso => Eprox <= espera_giro_servomotor;
 
@@ -81,11 +92,19 @@ begin
     o_conta_espera_giro_servomotor <= '1' when espera_giro_servomotor, '0' when others;
 
   with Eatual select
+    o_medir <= '1' when trigga_medida, '0' when others;
+
+  with Eatual select
+    o_repousando <= '1' when repouso, '0' when others;
+
+  with Eatual select
     db_estado <=
     "0000" when inicial,
-    "0001" when repouso,
-    "0010" when rega,
-    "0011" when alterna_vaso,
-    "0100" when espera_giro_servomotor,
+    "0001" when trigga_medida,
+    "0010" when espera_medida,
+    "0011" when repouso,
+    "0100" when rega,
+    "0101" when alterna_vaso,
+    "0110" when espera_giro_servomotor,
     "1111" when others;
 end arch;
