9b7c780c2c38c65b8e8f9a1414e857ee4843c3cc
Lucas Mafra <lucas.mafra95@gmail.com>
2020-11-26 16:06:34 -0300

    Add serial receiver to sistema_rega

--- sistema_rega.vhd
+++ sistema_rega.vhd
@@ -9,80 +9,47 @@ entity sistema_rega is
   port (
     i_clock: in std_logic;
     i_reset: in std_logic;
-    i_echo_sensor_0: in std_logic;
-    i_echo_sensor_1: in std_logic;
-    i_ligar: in std_logic;
-    o_abre_valvula: out std_logic;
-    o_pwm: out std_logic;
-    o_trigger_sensor_0: out std_logic;
-    o_trigger_sensor_1: out std_logic
+    i_umidade_solo_0: in std_logic;
+    i_threshold_1: in std_logic;
+    o_threshold_1: out std_logic;
+    o_display_0: out std_logic_vector(6 downto 0);
+    o_display_1: out std_logic_vector(6 downto 0);
+    o_display_2: out std_logic_vector(6 downto 0)      
   );
 end entity;
 
 architecture arch of sistema_rega is
+
+  component rx_serial is
+    port(
+      i_clock, i_reset: in std_logic;
+      i_dado_serial: in std_logic;
+      i_recebe_dado: in std_logic;
+      o_dado_recebido: out std_logic_vector (7 downto 0);
+      o_tem_dado: out std_logic;
+      o_pronto: out std_logic;	
+      db_recebe_dado: out std_logic;
+      db_dado_serial: out std_logic;
+      db_estado: out std_logic_vector(3 downto 0)
+    );
+  end component;
+
   
-  component controle_rega is
-    generic (
-      constant velocidade_simulacao: integer := 1
-    );
-    port (
-      i_clock: in std_logic;
-      i_reset: in std_logic;
-      i_ligar: in std_logic;
-      i_echo_sensor_0: in std_logic;
-      i_echo_sensor_1: in std_logic;
-      o_abre_valvula: out std_logic;
-      o_vaso: out std_logic_vector(1 downto 0);
-      o_trigger_sensor_0: out std_logic;
-      o_trigger_sensor_1: out std_logic
-    );
-  end component;
-
-  component controle_servo is
-    generic (
-      constant velocidade_simulacao: integer := 1
-    );
-    port (
-      i_clock    : in  std_logic;
-      i_reset    : in  std_logic;
-      i_posicao  : in  std_logic_vector(1 downto 0);  
-      o_pwm      : out std_logic;
-      db_reset   : out std_logic;
-      db_pwm     : out std_logic;
-      db_posicao : out std_logic_vector(1 downto 0)
-    );  
-  end component;
-
-  signal s_vaso: std_logic_vector(1 downto 0);
-
 begin
-  controle_rega_0: controle_rega
-    generic map (
-      velocidade_simulacao => velocidade_simulacao
-    )
-    port map (
-      i_clock => i_clock,
-      i_reset => i_reset,
-      i_ligar => i_ligar,
-      i_echo_sensor_0 => i_echo_sensor_0,
-      i_echo_sensor_1 => i_echo_sensor_1,
-      o_abre_valvula => o_abre_valvula,
-      o_vaso => s_vaso,
-      o_trigger_sensor_0 => o_trigger_sensor_0,
-      o_trigger_sensor_1 => o_trigger_sensor_1
-    );
-      
-  controle_servo_0: controle_servo
-    generic map (
-      velocidade_simulacao => velocidade_simulacao
-    )
-    port map (
-      i_clock => i_clock,
-      i_reset => i_reset,
-      i_posicao => s_vaso,
-      o_pwm => o_pwm,
-      db_reset => open,
-      db_pwm => open,
-      db_posicao => open
-    );
+  o_threshold_1 <= i_threshold_1;
+
+  u_rx_serial: rx_serial
+  port map (
+    i_clock => i_clock,
+    i_reset => i_reset,
+    i_dado_serial => i_umidade_solo_0,
+    i_recebe_dado => '1',
+    o_dado_recebido => open,
+    o_tem_dado => open,
+    o_pronto => open,
+    db_recebe_dado => open,
+    db_dado_serial => open,
+    db_estado => open
+  );
+  
 end arch;
--- sistema_rega_tb.vhd
+++ sistema_rega_tb.vhd
@@ -13,47 +13,44 @@ architecture tb of sistema_rega_tb is
     port (
       i_clock: in std_logic;
       i_reset: in std_logic;
-      i_ligar: in std_logic;
-      i_echo_sensor_0: in std_logic;
-      i_echo_sensor_1: in std_logic;
-      o_abre_valvula: out std_logic;
-      o_pwm: out std_logic;
-      o_trigger_sensor_0: out std_logic;
-      o_trigger_sensor_1: out std_logic
+      i_umidade_solo_0: in std_logic;
+      i_threshold_1: in std_logic;
+      o_threshold_1: out std_logic;
+      o_display_0: out std_logic_vector(6 downto 0);
+      o_display_1: out std_logic_vector(6 downto 0);
+      o_display_2: out std_logic_vector(6 downto 0)      
     );    
   end component;
 
-  signal clk_in: std_logic := '0';
-  signal rst_in: std_logic := '0';
-  signal ligar_in: std_logic := '0';
-  signal echo_sensor_0_in: std_logic := '0';
-  signal echo_sensor_1_in: std_logic := '0';
-  signal abre_valvula_out: std_logic;
-  signal pwm_out: std_logic;
-  signal trigger_sensor_0_out: std_logic;
-  signal trigger_sensor_1_out: std_logic;
+  signal i_clock: std_logic := '0';
+  signal i_reset: std_logic := '0';
+  signal i_umidade_solo_0: std_logic := '1';
+  signal i_threshold_1: std_logic := '0';
+  signal o_threshold_1: std_logic;
+  signal o_display_0: std_logic_vector(6 downto 0);
+  signal o_display_1: std_logic_vector(6 downto 0);
+  signal o_display_2: std_logic_vector(6 downto 0);
   signal simulando: std_logic := '0';    -- delimita o tempo de geração do clock
   constant periodoClock : time := 20 ns;  --  clock de 50MHz
-  
+
 begin
   -- Gerador de clock: executa enquanto 'simulando = 1', com o período especificado. 
   -- Quando simulando=0, clock é interrompido, bem como a simulação de eventos
-  clk_in <= (simulando and (not clk_in)) after periodoClock/2;
+  i_clock <= (simulando and (not i_clock)) after periodoClock/2;
     
   dut: sistema_rega
     generic map (
       velocidade_simulacao => 1000
     )
     port map (
-      i_clock => clk_in,
-      i_reset => rst_in,
-      i_ligar => ligar_in,
-      i_echo_sensor_0 => echo_sensor_0_in,
-      i_echo_sensor_1 => echo_sensor_1_in,
-      o_abre_valvula => abre_valvula_out,
-      o_pwm => pwm_out,
-      o_trigger_sensor_0 => trigger_sensor_0_out,
-      o_trigger_sensor_1 => trigger_sensor_1_out
+      i_clock => i_clock,
+      i_reset => i_reset,
+      i_umidade_solo_0 => i_umidade_solo_0,
+      i_threshold_1 => i_threshold_1,
+      o_threshold_1 => o_threshold_1,
+      o_display_0 => o_display_0,
+      o_display_1 => o_display_1,
+      o_display_2 => o_display_2
     );
 
   stimulus: process is
@@ -61,68 +58,128 @@ begin
     assert false report "Inicio da simulacao" severity note;
     simulando <= '1';
     
-    rst_in <= '1';
-    wait for periodoClock;    
-    assert abre_valvula_out = '0' report "reset - abre_valvula = 0";
-    rst_in <= '0';
+    i_reset <= '1';
+    wait for 100* periodoClock;    
+    i_reset <= '0';
 
-    wait for periodoClock;
+    wait for 10*periodoClock;
 
-    ligar_in <= '1';
-    wait for periodoClock;
-    ligar_in <= '0';
+    -- COMECA TRANSMISSAO DO CARACTERE 8
+    i_umidade_solo_0 <= '0'; -- start bit do primeiro caractere (0x38);
+    wait for 434*periodoClock;
 
-    wait for periodoClock;
-    assert trigger_sensor_0_out = '1' report "should trigger sensor 0";
-    assert trigger_sensor_1_out = '0' report "should not trigger sensor 1";
-    wait for 10 us; -- espera fim do sensor trigger
+    i_umidade_solo_0 <= '0'; -- digito menos significativo de 0x38;
+    wait for 434*periodoClock;
 
-    echo_sensor_0_in <= '1';
-    wait for 150 us; -- 75% de umidade
-    echo_sensor_0_in <= '0';
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
 
-    wait for 2*periodoClock;
-    assert abre_valvula_out = '1' report "abriu valvula";
+    i_umidade_solo_0 <= '0';
+  wait for 434*periodoClock;
 
-    wait for 1 ms;
-    assert abre_valvula_out = '0' report "fechou valvula";
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
 
-    wait for 0.9 ms;
-    assert abre_valvula_out = '0' report "valvula fechada enquanto repousa";
-    assert trigger_sensor_0_out = '0' report "nao trigga sensor 0 enquanto repousa";
-    assert trigger_sensor_1_out = '0' report "nao trigga sensor 1 enquanto repousa";
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
 
-    wait for 0.1 ms;
-    wait for periodoClock;
-    assert trigger_sensor_0_out = '1' report "mede sensor 0 novamente";
-    wait for 10 us; -- espera fim do sensor trigger
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
 
-    echo_sensor_0_in <= '1';
-    wait for 190 us; -- 95% de umidade
-    echo_sensor_0_in <= '0';
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
 
-    wait for 2*periodoClock;
-    assert abre_valvula_out = '0' report "nao deve mais regar vaso 00";
+    i_umidade_solo_0 <= '0'; -- digito mais significativo de 0x38;
+    wait for 434*periodoClock;
 
-    wait for 1.5 ms; -- repouso + giro servo motor
+    i_umidade_solo_0 <= '1'; -- primeiro stop bit de 0x38
+    wait for 434*periodoClock;
 
-    wait for 2*periodoClock;
+    i_umidade_solo_0 <= '1'; -- segundo stopbit de 0x38
+    wait for 434*periodoClock;
+    -- FINALIZA TRANSMISSAO DO CARACTERE 8
+
+
+    -- COMECA TRANSMISSAO DO CARACTERE 5
+    i_umidade_solo_0 <= '0'; -- start bit do primeiro caractere (0x35);
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1'; -- digito menos significativo de 0x35;
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0'; -- digito mais significativo de 0x35;
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1'; -- primeiro stop bit de 0x35
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1'; -- segundo stopbit de 0x35
+    wait for 434*periodoClock;
+    -- FINALIZA TRANSMISSAO DO CARACTERE 5
+
+
+    -- COMECA TRANSMISSAO DO CARACTERE 0
+    i_umidade_solo_0 <= '0'; -- start bit do primeiro caractere (0x30);
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0'; -- digito menos significativo de 0x30;
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0';
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '0'; -- digito mais significativo de 0x30;
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1'; -- primeiro stop bit de 0x30
+    wait for 434*periodoClock;
+
+    i_umidade_solo_0 <= '1'; -- segundo stopbit de 0x30
+    wait for 434*periodoClock;
+    -- FINALIZA TRANSMISSAO DO CARACTERE 0
     
-    assert trigger_sensor_0_out = '0' report "should not trigger sensor 0";
-    assert trigger_sensor_1_out = '1' report "should trigger sensor 1";
-    wait for 10 us; -- espera fim do sensor trigger
 
-    echo_sensor_1_in <= '1';
-    wait for 180 us; -- 90% de umidade
-    echo_sensor_1_in <= '0';
+    wait for 1000*periodoClock;
 
-    wait for 2*periodoClock;
-    assert abre_valvula_out = '0' report "nao deve regar vaso 01";
-
-    wait for 1.5 ms; -- repouso + giro servo motor
-
+    assert o_display_0 = "0000000" report "Display 0 = 8" severity error;
+    assert o_display_1 = "0010010" report "Display 1 = 5" severity error;
+    assert o_display_2 = "1000000" report "Display 2 = 0" severity error;
+      
     assert false report "Fim da simulacao" severity note;
     simulando <= '0';
     wait; -- fim da simulação
-  end process;
+  end process;  
 end tb;
